0ac395ee177280479c5ba43bd9fd5b3d9e1ec5c8
LeeChiuLan <magicq2005@yaoo.com.tw>
2020-01-04 21:27:13 +0800

    add G10716001_dynamic_assess.html

---
+++ qdatabase.js
@@ -0,0 +1,94 @@
+﻿//Dynamic assessmentitem bank for G10716001_dynamic_assess.html
+var bank1=[];
+bank1[0]='那個惡意軟體會開啟被遠端主機控制的應用程式?';
+bank1[1]='那個惡意軟體會在你不知情下就安裝完成?';
+bank1[2]='癱瘓網站何者為是?';
+bank1[3]='何者是DDoS?';
+bank1[4]='下列何者可以取得管理者帳號密碼?';
+bank1[5]='某網站貼出公告[網頁遭惡意攻擊課程資料庫損毀目前搶修中不便之處敬請見諒],這是遭受什麼樣的資安攻擊?';
+bank1[6]='下列何者會提高被惡意軟體攻擊的風險?';
+bank1[7]='那個行為可以防範資安危險?';
+bank1[8]='駭客網路入侵模式,那個行為可以防範[弱點刺探]?';
+bank1[9]='行動裝置何者徵兆有惡意軟體入侵?';
+var sItems=[];
+	sItems[0] = "木馬程式";
+	sItems[1] = "後門程式";
+	sItems[2] = "電腦蠕蟲";
+	sItems[3] = "釣魚郵件";
+	sItems[4] = "釣魚郵件";
+	sItems[5] = "釣魚網站";
+	sItems[6] = "安裝套件軟體";
+	sItems[7] = "以上皆是";
+	sItems[8] = "對於SQL伺服器做破壞攻擊";
+	sItems[9] = "發動大量攻擊封包";
+	sItems[10] = "植入電腦病毒";
+	sItems[11] = "竊取大量資料";
+	sItems[12] = "指被藉機勒索";
+	sItems[13] = "一旦參觀網站就會被攻擊或被感染惡意程式";
+	sItems[14] = "機密資料會被網路罪犯偷走";
+	sItems[15] = "癱瘓網站";
+	sItems[16] = "隱碼攻擊";
+	sItems[17] = "側錄與竊聽";
+	sItems[18] = "勒索病毒";
+	sItems[19] = "釣魚郵件";
+	sItems[20] = "電腦蠕蟲";
+	sItems[21] = "勒索病毒";
+	sItems[22] = "SQL Injection攻擊";
+	sItems[23] = "癱瘓網站攻擊";
+	sItems[24] = "安裝即時軟體";
+	sItems[25] = "安裝P2P軟體";
+	sItems[26] = "安裝防毒軟體";
+	sItems[27] = "安裝系統更新軟體";
+	sItems[28] = "禁止使用USB隨身碟";
+	sItems[29] = "瀏覽高風險的網站";
+	sItems[30] = "禁止使用即時軟體";
+	sItems[31] = "以上皆非";
+	sItems[32] = "勿使用P2P軟體";
+	sItems[33] = "謹慎開啟信件附加檔案";
+	sItems[34] = "定期更新修補弱點";
+	sItems[35] = "安裝防火牆";
+	sItems[36] = "通話經常不尋常中斷";
+	sItems[37] = "電信費用異常";
+	sItems[38] = "行動裝置效能變差";
+	sItems[39] = "以上皆是";
+var ans = [1, 3, 1, 3, 0, 2, 1, 0, 2, 3];
+
+var prompt1=[];
+prompt1[0]="注意for第一段執行次數";
+prompt1[1]="for第二段之條件";
+prompt1[2]="注意for執行順序";
+prompt1[3]="for第一段只執行1次，for&nbsp;body執行10次";
+prompt1[4]="*注意for第一段執行次數";
+prompt1[5]="*for第二段之條件";
+prompt1[6]="*注意for執行順序，是否為空?圈";
+prompt1[7]="*for第一段只執行1次，for body執行0次";
+prompt1[8]="3注意for第一段執行次數";
+prompt1[9]="3for第二段之條件";
+prompt1[10]="3注意for執行順序";
+prompt1[11]="3for第一段只執行1次，for&nbsp;body執行10次";
+prompt1[12]="4*注意for第一段執行次數";
+prompt1[13]="4*for第二段之條件";
+prompt1[14]="4*注意for執行順序，是否為空?圈";
+prompt1[15]="4*for第一段只執行1次，for body執行0次";
+
+//int x=19, y=-1;
+//for(int i=1,y++;i<=10;i++) --x;
+//System.out.print(y+","+x);
+//印出結果?
+//注意for第一段執行次數
+//for第二段之條件
+//注意for執行順序
+//for第一段只執行1次，for body執行10次
+
+//int x=8, y=2, i=6;
+//for(i=1,--y;i>=6;--i) --x;
+//System.out.print(i+","+x);
+//印出結果?
+//0,2
+//2,2
+//1,8
+//6,8
+//注意for第一段執行次數
+//for第二段之條件
+//注意for執行順序，是否為空?圈
+//for第一段只執行1次，for body執行0次
